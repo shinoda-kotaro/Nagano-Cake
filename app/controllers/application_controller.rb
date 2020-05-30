3b4f35812f6423d98978c7891bd65964ef9263e8
shinoda-kotaro <shinoda.kotaro.infratop@gmail.com>
2020-05-30 15:54:21 +0900

    [fix] admin logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,18 @@ class ApplicationController < ActionController::Base
     elsif current_end_user
       end_users_mypage_path(current_end_user)
     else
-      
+      root_path
     end
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :end_user
+      root_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
