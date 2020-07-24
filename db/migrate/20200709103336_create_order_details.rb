8d03a7afda45f4b34446f2b9440dec6e180303be
shinoda-kotaro <shinoda.kotaro.infratop@gmail.com>
2020-07-24 19:20:45 +0900

    fix fk of order_details table

--- db/migrate/20200709103336_create_order_details.rb
+++ db/migrate/20200709103336_create_order_details.rb
@@ -3,7 +3,7 @@ class CreateOrderDetails < ActiveRecord::Migration[5.2]
     create_table :order_details do |t|
 
       t.integer :item_id , null: false
-      t.references :order, foreign_key: true
+      t.references :order
       t.integer :price , null: false
       t.integer :production_status , null: false , default: 0
       t.integer :amount , null: false
